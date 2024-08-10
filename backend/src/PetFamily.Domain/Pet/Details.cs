436911095f74973ee2482ef20759701bb4321d58
AntonZhikin <antonzhikin0@gmail.com>
2024-08-10 19:01:05 +0300

    Create Pet

---
+++ backend/src/PetFamily.Domain/Pet/Details.cs
@@ -0,0 +1,8 @@
+namespace PetFamily.Domain.Pet;
+
+public class Details
+{
+    public string Name { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+}
---
+++ backend/src/PetFamily.Domain/Pet/Pet.cs
@@ -0,0 +1,38 @@
+﻿namespace PetFamily.Domain.Pet;
+
+public class Pet
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    
+    public string King { get; set; } = null!;
+    
+    public string Description { get; set; } = null!;
+    
+    public string Breed { get; set; } = null!;
+
+    public string Color { get; set; } = null!;
+
+    public string PetHealthInfo { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public int Weight { get; set; } = 0!;
+
+    public int Height { get; set; } = 0!;
+    
+    public decimal PhoneNumber { get; set; }
+    
+    public bool Neutered { get; set; }
+    
+    public DateOnly DateOfBirth { get; set; }
+    
+    public bool Vactine { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public List<Details> Detail { get; set; } = [];
+
+    public DateOnly DateCreate { get; set; }
+}
