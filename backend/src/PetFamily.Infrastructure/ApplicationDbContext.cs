82cda7caf1a10db5b4189cbce90fbf1bb8b232d4
AntonZhikin <antonzhikin0@gmail.com>
2024-08-15 19:57:49 +0300

    добавил сикритс и гитигнор вроде исправил

--- backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
+++ backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using PetFamily.Domain.Pet;
 
 namespace PetFamily.Infrastructure;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(IConfiguration configuration) : DbContext
 {
     public DbSet<Pet> Pets => Set<Pet>();
 
+    private const string DATABASE = "Database";
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Server=localhost;Port=5434;Database=pet_family;User Id=postgres;Password=postgres;");
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
         optionsBuilder.UseSnakeCaseNamingConvention();
         optionsBuilder.UseLoggerFactory(CreateLoggerFactory());
     }
-
+//"Server=localhost;Port=5434;Database=pet_family;User Id=postgres;Password=postgres;"
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
