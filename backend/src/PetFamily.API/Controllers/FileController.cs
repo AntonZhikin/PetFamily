f6b8ecdd390188abd4e3a95242770e0768d69899
AntonZhikin <antonzhikin0@gmail.com>
2024-11-28 13:31:49 +0300

    первая часть minio

---
+++ backend/src/PetFamily.API/Controllers/FileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetFamily.API.Controllers;
+
+public class FileController : ApplicationController
+{
+    [HttpPost]
+
+    public async Task<IActionResult> CreateFile([FromForm] IFormFile file)
+    {
+        return Ok(file);
+    }
+}
