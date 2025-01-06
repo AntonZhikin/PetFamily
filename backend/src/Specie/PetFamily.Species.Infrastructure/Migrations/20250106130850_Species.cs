964aeb9aeffc7e1bd3ed9a9e0eb27c9b80686592
AntonZhikin <antonzhikin0@gmail.com>
2025-01-07 01:17:58 +0300

    unversioned

---
+++ backend/src/Specie/PetFamily.Species.Infrastructure/Migrations/20250106130850_Species.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Species.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Species : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "PetFamily_Species");
+
+            migrationBuilder.CreateTable(
+                name: "species",
+                schema: "PetFamily_Species",
+                columns: table => new
+                {
+                    species_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_species", x => x.species_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "breed",
+                schema: "PetFamily_Species",
+                columns: table => new
+                {
+                    breed_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    species_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_breed", x => x.breed_id);
+                    table.ForeignKey(
+                        name: "fk_breed_species_species_id",
+                        column: x => x.species_id,
+                        principalSchema: "PetFamily_Species",
+                        principalTable: "species",
+                        principalColumn: "species_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_breed_species_id",
+                schema: "PetFamily_Species",
+                table: "breed",
+                column: "species_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "breed",
+                schema: "PetFamily_Species");
+
+            migrationBuilder.DropTable(
+                name: "species",
+                schema: "PetFamily_Species");
+        }
+    }
+}
